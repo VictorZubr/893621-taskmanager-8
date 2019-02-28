0c0a5bbc97540cad0419736c2859a36d116a9ff9
VictorZubr <zubr@rambler.ru>
2019-02-28 16:58:41 +0300

    Выделил констаны, сделал шаблон выбора цвета карточки

--- src/get-task.js
+++ src/get-task.js
@@ -1,46 +1,25 @@
 import {getRandomInteger, getRandomItems, getTrueOrFalse} from "./utils";
 
 const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
-
 const WEEK = 7;
+const TITLES = [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`, `Составить отзыв об интенсиве`];
+const HASTAGS = [`homework`, `theory`, `practice`, `intensive`, `keks`, `JavaScript`, `family`, `shopping`];
+const WEEK_DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
 
-const titles = [
-  `Изучить теорию`,
-  `Сделать домашку`,
-  `Пройти интенсив на соточку`,
-  `Составить отзыв об интенсиве`
-];
-
-const colors = [
-  `black`,
-  `yellow`,
-  `blue`,
-  `green`,
-  `pink`
-];
-
-const hashtags = [
-  `homework`,
-  `theory`,
-  `practice`,
-  `intensive`,
-  `keks`,
-  `JavaScript`,
-  `family`,
-  `shopping`,
-];
-
+export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
 export default () => ({
-  title: titles[getRandomInteger(0, titles.length - 1)],
+  title: TITLES[getRandomInteger(0, TITLES.length - 1)],
   dueDate: Date.now() - WEEK * MILLISECONDS_IN_DAY + getRandomInteger(0, 2 * WEEK) * MILLISECONDS_IN_DAY, // плюс минус 7 дней от текущей даты
-  tags: new Set(getRandomItems(hashtags, getRandomInteger(0, 3))),
+  tags: new Set(getRandomItems(HASTAGS, getRandomInteger(0, 3))),
   picture: `//picsum.photos/100/100?r=${Math.random()}`,
-  color: colors[getRandomInteger(0, colors.length - 1)],
+  color: COLORS[getRandomInteger(0, COLORS.length - 1)],
 
   // При формировании дней повторения понизил вероятность (0.2) в сторону false, чтобы иногда формировались задачи без повторений
 
-  repeatingDays: [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`].reduce((acc, element) => {
+  repeatingDays: WEEK_DAYS.reduce((acc, element) => {
     acc[element] = getTrueOrFalse(0.2);
     return acc;
-  }, {})
+  }, {}),
+  isFavorite: getTrueOrFalse(),
+  isDone: getTrueOrFalse()
 });
--- src/make-task.js
+++ src/make-task.js
@@ -1,3 +1,5 @@
+import {COLORS} from "./get-task";
+
 const getFormattedDate = (milliseconds) => {
   const date = new Date(milliseconds);
   return `${date.toLocaleString(`en-US`, {day: `2-digit`})} ${date.toLocaleString(`en-US`, {month: `long`})}`;
@@ -40,6 +42,21 @@ const getRepeatingDaysHTML = (days) =>
                             ${(days[element]) ? `checked` : ``}
                     />
                     <label class="card__repeat-day" for="repeat-${element}-2">${element}</label>`)
+  .join(``);
+
+// Функция возвращает шаблон контейнера для выбора цвета карточки
+
+const getCardColorsWrapHTML = (colors, task) =>
+  colors
+  .map((element) => `<input
+                        type="radio"
+                        id="color-${element}-2"
+                        class="card__color-input card__color-input--${element} visually-hidden"
+                        name="color"
+                        value="${element}"
+                        ${(element === task.color) ? `checked` : ``}
+                     />
+                     <label for="color-${element}-2" class="card__color card__color--${element}">${element}</label>`)
   .join(``);
 
 // экспортируем функцию, которая возвращает шаблон одной задачи
@@ -139,68 +156,7 @@ ${task.title}</textarea
                             <div class="card__colors-inner">
                                 <h3 class="card__colors-title">Color</h3>
                                 <div class="card__colors-wrap">
-                                    <input
-                                            type="radio"
-                                            id="color-black-2"
-                                            class="card__color-input card__color-input--black visually-hidden"
-                                            name="color"
-                                            value="black"
-                                    />
-                                    <label
-                                            for="color-black-2"
-                                            class="card__color card__color--black"
-                                    >black</label
-                                    >
-                                    <input
-                                            type="radio"
-                                            id="color-yellow-2"
-                                            class="card__color-input card__color-input--yellow visually-hidden"
-                                            name="color"
-                                            value="yellow"
-                                    />
-                                    <label
-                                            for="color-yellow-2"
-                                            class="card__color card__color--yellow"
-                                    >yellow</label
-                                    >
-                                    <input
-                                            type="radio"
-                                            id="color-blue-2"
-                                            class="card__color-input card__color-input--blue visually-hidden"
-                                            name="color"
-                                            value="blue"
-                                    />
-                                    <label
-                                            for="color-blue-2"
-                                            class="card__color card__color--blue"
-                                    >blue</label
-                                    >
-                                    <input
-                                            type="radio"
-                                            id="color-green-2"
-                                            class="card__color-input card__color-input--green visually-hidden"
-                                            name="color"
-                                            value="green"
-                                    />
-                                    <label
-                                            for="color-green-2"
-                                            class="card__color card__color--green"
-                                    >green</label
-                                    >
-                                    <input
-                                            type="radio"
-                                            id="color-pink-2"
-                                            class="card__color-input card__color-input--pink visually-hidden"
-                                            name="color"
-                                            value="pink"
-                                            checked
-                                    />
-                                    <label
-                                            for="color-pink-2"
-                                            class="card__color card__color--pink"
-                                    >pink</label
-                                    >
-                                </div>
+                                    ${getCardColorsWrapHTML(COLORS, task)}
                             </div>
                         </div>
                         <div class="card__status-btns">
